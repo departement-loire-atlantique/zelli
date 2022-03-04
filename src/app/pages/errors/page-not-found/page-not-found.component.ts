61bfc93ce23e6f6d7024fd8eef048315bcf4bce8
porhon <paul.orhon@digiwin.fr>
2022-03-04 17:16:22 +0100

    Maj css color bg

--- src/app/pages/errors/page-not-found/page-not-found.component.ts
+++ src/app/pages/errors/page-not-found/page-not-found.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.less'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./page-not-found.component.less']
 })
 export class PageNotFoundComponent implements OnInit {
 
