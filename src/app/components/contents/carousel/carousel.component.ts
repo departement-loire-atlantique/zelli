8818719bb42e9f009fa7a43fe666fdfd0521b702
porhon <paul.orhon@digiwin.fr>
2022-03-25 18:03:35 +0100

    Carousel

--- src/app/components/contents/carousel/carousel.component.ts
+++ src/app/components/contents/carousel/carousel.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Carousel } from 'src/app/models/jcms/carousel';
+import { Carousel, CarouselElement } from 'src/app/models/jcms/carousel';
 import { JcmsClientService } from 'src/app/services/jcms-client.service';
+import { environment } from 'src/environments/environment';
+
+/**
+ * Class JS du DS 44
+ */
+declare class CarouselStandard{};
 
 @Component({
   selector: 'app-carousel',
@@ -18,11 +24,34 @@ export class CarouselComponent implements OnInit {
   @Input("text")
   diaporamaTexte: string | undefined;
 
+  elements: CarouselElement[] = [];
+
   constructor(private _jcms: JcmsClientService) { }
 
   ngOnInit(): void {
-    if(!this.carousel && this.id){
-      this._jcms.get<Carousel>("data/"+ this.id).subscribe((res : Carousel) => this.carousel = res);
+    if (!this.carousel && this.id) {
+      this._jcms.get<Carousel>("data/" + this.id).subscribe((res: Carousel) => {
+        this.carousel = res;
+        this.getFullElement();
+      });
+    } else {
+      this.getFullElement();
+    }
+
+  }
+
+  private getFullElement() {
+    if(!this.carousel || !this.carousel.elements1){
+      return;
+    }
+    for (let item of this.carousel.elements1) {
+      this._jcms.get<CarouselElement>("data/" + item.id).subscribe((res: CarouselElement) => {
+        // TODO service fix img link
+        res.imageMobile = environment.urlJcms + res.imageMobile;
+        this.elements.push(res);
+
+        new CarouselStandard();
+      });
     }
   }
 
