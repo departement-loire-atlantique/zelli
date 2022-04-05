b1fa4216d7ca3a513e4c27bc381e4b511f5c81be
porhon <paul.orhon@digiwin.fr>
2022-04-05 17:09:49 +0200

    Maj lbl service

--- src/app/services/label-mng.service.ts
+++ src/app/services/label-mng.service.ts
@@ -1,31 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { JcmsClientService } from './jcms-client.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LabelMngService {
+  private _lbls: lbl[] = [
+    {
+      id: 'lblTrieur',
+      lbl: 'Les documents à garder dans ton trieur',
+      propJcms: 'jcmsplugin.zelli.lbl.btn.contenu.trieur',
+    },
+    {
+      id: 'lblMotsComp',
+      lbl: 'Les mots compliqués',
+      propJcms: 'jcmsplugin.zelli.lbl.mots.compliques',
+    },
+  ];
 
-  private _lblDocTrieur: string = "Les documents à garder dans ton trieur";
-  private _propDocTrieur: string = "jcmsplugin.zelli.lbl.btn.contenu.trieur";
-
-  constructor(private _jcms: JcmsClientService) {
-  }
+  constructor(private _jcms: JcmsClientService) {}
 
   public initAllLbl(): any {
-    return {"lblTrieur": this.updateLblDocTrieur()};
+    let rep: any = {};
+    for (let itLbl of this._lbls) {
+      rep[itLbl.id] = this.updateLbl(itLbl);
+    }
+    return rep;
   }
 
-  public updateLblDocTrieur(): Observable<any> {
-    const lbl = this._jcms.get("admin/property/" + this._propDocTrieur);
-    lbl.subscribe((rep: any) =>
-      this._lblDocTrieur = rep.value
-    );
-    return lbl;
+  public updateLbl(lbl: lbl): Observable<any> {
+    const obs = this._jcms.get('admin/property/' + lbl.propJcms);
+    obs.subscribe((rep: any) => (lbl.lbl = rep.value));
+    return obs;
   }
 
-  public get lblDocTrieur(): string {
-    return this._lblDocTrieur;
+  public lblDocTrieur(): string {
+    return this._lbls[0].lbl;
   }
+
+  public lblMotComp(): string {
+    return this._lbls[1].lbl;
+  }
+}
+
+interface lbl {
+  id: string;
+  lbl: string;
+  propJcms: string;
 }
