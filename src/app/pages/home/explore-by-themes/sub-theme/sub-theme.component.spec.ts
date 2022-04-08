a7dba60d89e0a7d460127d590186546f25e2fa1b
Renan <renan.decamps@gmail.com>
2022-04-08 11:37:58 +0200

    fix: sub-theme component tests

--- src/app/pages/home/explore-by-themes/sub-theme/sub-theme.component.spec.ts
+++ src/app/pages/home/explore-by-themes/sub-theme/sub-theme.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { from } from 'rxjs';
+
+import { CatsMngService } from '@/app/services/cats-mng.service';
+import { JcmsClientService } from '@/app/services/jcms-client.service';
 
 import { SubThemeComponent } from './sub-theme.component';
 
@@ -8,9 +13,32 @@ describe('SubThemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SubThemeComponent ]
-    })
-    .compileComponents();
+      declarations: [SubThemeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => 'subId',
+              },
+            },
+          },
+        },
+        {
+          provide: CatsMngService,
+          useValue: {
+            cat: () => from([]),
+          },
+        },
+        {
+          provide: JcmsClientService,
+          useValue: {
+            get: () => from([]),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
