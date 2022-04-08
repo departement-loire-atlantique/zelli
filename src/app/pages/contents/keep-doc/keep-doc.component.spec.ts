ba8a9198a5001062f0b77215f10a72a006faab9e
Renan <renan.decamps@gmail.com>
2022-04-08 10:39:40 +0200

    fix: keep-doc component tests

--- src/app/pages/contents/keep-doc/keep-doc.component.spec.ts
+++ src/app/pages/contents/keep-doc/keep-doc.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { from } from 'rxjs';
+
+import { CatsMngService } from '@/app/services/cats-mng.service';
+import { JcmsClientService } from '@/app/services/jcms-client.service';
 
 import { KeepDocComponent } from './keep-doc.component';
 
@@ -8,9 +13,28 @@ describe('KeepDocComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ KeepDocComponent ]
-    })
-    .compileComponents();
+      declarations: [KeepDocComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: from([]),
+          },
+        },
+        {
+          provide: JcmsClientService,
+          useValue: {
+            get: () => from([]),
+          },
+        },
+        {
+          provide: CatsMngService,
+          useValue: {
+            cat: () => from([]),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
