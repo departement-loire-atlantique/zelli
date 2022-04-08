83c5214ee262a51d3b1103065972adb14ab6a9aa
Renan <renan.decamps@gmail.com>
2022-04-08 11:37:46 +0200

    fix: age component tests

--- src/app/pages/home/age/age.component.spec.ts
+++ src/app/pages/home/age/age.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+
+import { CatsHomeMngService } from '@/app/services/cats-home-mng.service';
 
 import { AgeComponent } from './age.component';
 
@@ -8,9 +11,32 @@ describe('AgeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AgeComponent ]
-    })
-    .compileComponents();
+      declarations: [AgeComponent],
+      providers: [
+        {
+          provide: Router,
+          useValue: {
+            url: '/url',
+          },
+        },
+        {
+          provide: CatsHomeMngService,
+          useValue: {
+            getCatFromUrl: () => ({
+              id: 'id',
+              title: 'title',
+              smallTitle: 'smallTitle',
+              subTitle: 'subTitle',
+              icon: 'icon',
+              image: 'image',
+              url: 'url',
+              order: 0,
+              idContentTrieur: 'idContentTrieur',
+            }),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
