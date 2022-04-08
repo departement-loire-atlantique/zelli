492e7748b4a71d7497a6015dba1770508078e708
Renan <renan.decamps@gmail.com>
2022-04-08 10:35:53 +0200

    fix: carousel component tests

--- src/app/components/contents/carousel/carousel.component.spec.ts
+++ src/app/components/contents/carousel/carousel.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { from } from 'rxjs';
+
+import { JcmsClientService } from '@/app/services/jcms-client.service';
 
 import { CarouselComponent } from './carousel.component';
 
@@ -8,9 +11,16 @@ describe('CarouselComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CarouselComponent ]
-    })
-    .compileComponents();
+      declarations: [CarouselComponent],
+      providers: [
+        {
+          provide: JcmsClientService,
+          useValue: {
+            get: () => from([]),
+          },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
