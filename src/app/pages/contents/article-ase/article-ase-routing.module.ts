1094cb47f3a248d49e4a88499d837516c91e3ad6
Renan <renan.decamps@gmail.com>
2022-04-14 15:09:46 +0200

    remove preview picture from video component

--- src/app/pages/contents/article-ase/article-ase-routing.module.ts
+++ src/app/pages/contents/article-ase/article-ase-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ArticleASEComponent } from './article-ase.component';
 
 const routes: Routes = [{ path: '', component: ArticleASEComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ArticleASERoutingModule { }
+export class ArticleASERoutingModule {}
