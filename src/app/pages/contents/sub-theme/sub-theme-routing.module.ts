ba0bcc0eacf7a83344e43352b31e52ec996280bd
porhon <paul.orhon@digiwin.fr>
2022-04-15 18:02:51 +0200

    start sub theme refont

--- src/app/pages/contents/sub-theme/sub-theme-routing.module.ts
+++ src/app/pages/contents/sub-theme/sub-theme-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { SubThemeComponent } from './sub-theme.component';
 
 const routes: Routes = [{ path: '', component: SubThemeComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SubThemeRoutingModule { }
+export class SubThemeRoutingModule {}
--- src/app/pages/contents/sub-theme/sub-theme.module.ts
+++ src/app/pages/contents/sub-theme/sub-theme.module.ts
@@ -1,17 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/components/shared.module';
+
 import { SubThemeRoutingModule } from './sub-theme-routing.module';
 import { SubThemeComponent } from './sub-theme.component';
 
 @NgModule({
-  declarations: [
-    SubThemeComponent
-  ],
-  imports: [
-    CommonModule,
-    SubThemeRoutingModule,
-    SharedModule
-  ]
+  declarations: [SubThemeComponent],
+  imports: [CommonModule, SubThemeRoutingModule, SharedModule],
 })
-export class SubThemeModule { }
+export class SubThemeModule {}
