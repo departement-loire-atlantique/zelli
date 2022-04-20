5499389829b025938de7820ce6bb804e4d8b69df
porhon <paul.orhon@digiwin.fr>
2022-04-20 15:47:29 +0200

    add form search

--- src/app/pages/research/research.component.ts
+++ src/app/pages/research/research.component.ts
@@ -1,14 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+
+import { Content } from '@/app/models/jcms/content';
+import { JcmsClientService } from '@/app/services/jcms-client.service';
+import { Util } from '@/app/util';
+
+import { environment } from '@/environments/environment';
+
+import { Item } from '../../components/list/list.component';
 
 @Component({
   selector: 'app-research',
   templateUrl: './research.component.html',
   styleUrls: ['./research.component.less'],
 })
-export class ResearchComponent implements OnInit {
-  constructor() {}
+export class ResearchComponent {
+  text: string = '';
 
-  ngOnInit(): void {
-    console.log('TODO');
+  researchRun: boolean = false;
+
+  result: Item[] = [];
+
+  constructor(private _jcms: JcmsClientService) {}
+
+  public research(): void {
+    if (!this.text) {
+      return;
+    }
+    this.researchRun = true;
+
+    this._jcms
+      .get('search', {
+        params: {
+          text: this.text,
+          types: ['SousthemeASE', 'ArticleASE', 'Contact', 'FileDocument'],
+          exactType: true,
+        },
+      })
+      .pipe(map((rep: any): Content[] => rep.dataSet))
+      .subscribe((contents: Content[]) => {
+        console.log(contents);
+        for (let itContent of contents) {
+          this.result.push({
+            lbl: itContent.title,
+            url: Util.buildUrlCotent(itContent),
+          });
+        }
+        this.researchRun = false;
+      });
   }
 }
