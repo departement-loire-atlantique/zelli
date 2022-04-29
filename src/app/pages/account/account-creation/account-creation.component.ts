630e2510b3a9eae455adb75a5b907b19bdc83231
porhon <paul.orhon@digiwin.fr>
2022-04-29 17:59:47 +0200

    render titles

--- src/app/pages/account/account-creation/account-creation.component.ts
+++ src/app/pages/account/account-creation/account-creation.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LabelMngService } from '@/app/services/label-mng.service';
+
 @Component({
   selector: 'app-account-creation',
   templateUrl: './account-creation.component.html',
   styleUrls: ['./account-creation.component.less'],
 })
 export class AccountCreationComponent implements OnInit {
-  constructor() {}
+  step: number = 1;
+
+  maxStep: number = 3;
+
+  titleByStep: string[] = [
+    'accountCreateStep1',
+    'accountCreateStep2',
+    'accountCreateStep3',
+  ];
+
+  constructor(public lblService: LabelMngService) {}
 
   ngOnInit(): void {
     console.log('TODO');
+  }
+
+  public nextStep() {
+    // TODO test curent step
+    if (this.step >= this.maxStep) {
+      // TODO end
+    }
+    this.step++;
+  }
+
+  public lblBtnNext(): string {
+    if (this.step >= this.maxStep) {
+      return 'Créer mon compte';
+    }
+    return 'Suivant';
   }
 }
