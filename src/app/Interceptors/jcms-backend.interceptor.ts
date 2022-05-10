d7a64460ee6affc81feab8e8fcb7e79636973de5
paul604 <paul604@users.noreply.github.com>
2022-05-10 15:20:40 +0200

    use login serivce in JcmsBackendInterceptor

--- src/app/Interceptors/jcms-backend.interceptor.ts
+++ src/app/Interceptors/jcms-backend.interceptor.ts
@@ -1,23 +1,35 @@
 import {
-  HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpParams, HttpRequest
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpParams,
+  HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+
+import { LoginService } from '../services/login.service';
 
 @Injectable()
 export class JcmsBackendInterceptor implements HttpInterceptor {
+  constructor(private _login: LoginService) {}
 
-  constructor() { }
-
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    let key = environment.apiKey;
-
-    // TODO get user key if login
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    let key = this._login.token;
 
     const newReq = request.clone({
-      headers: (request.headers ? request.headers : new HttpHeaders()).set("Accept", "application/json"),
-      params: (request.params ? request.params : new HttpParams()).set("authKey", key)
+      headers: (request.headers ? request.headers : new HttpHeaders()).set(
+        'Accept',
+        'application/json'
+      ),
+      params: (request.params ? request.params : new HttpParams()).set(
+        'authKey',
+        key
+      ),
     });
 
     return next.handle(newReq);
