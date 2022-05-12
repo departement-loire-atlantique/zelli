8b7ec1ab0de62b48e700f0248dd0df42bf9507c3
Renan <renan.decamps@gmail.com>
2022-05-12 17:49:26 +0200

    fix: has-helped sizing

--- src/app/components/has-helped/has-helped.stories.ts
+++ src/app/components/has-helped/has-helped.stories.ts
@@ -1,10 +1,16 @@
-import { Meta, Story } from '@storybook/angular';
+import { Meta, Story, moduleMetadata } from '@storybook/angular';
 
 import { HasHelpedComponent } from '@/app/components/has-helped/has-helped.component';
+import { IconComponent } from '@/app/components/icon/icon.component';
 
 export default {
   title: 'Components/HasHelped',
   component: HasHelpedComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [IconComponent],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<HasHelpedComponent> = (args: HasHelpedComponent) => ({
