a3183cfff3371ff07696cd62d3fc3c5a166dcfa5
paul604 <paul604@users.noreply.github.com>
2022-05-13 15:37:24 +0200

    add profile link

--- src/app/components/page-title/page-title.component.ts
+++ src/app/components/page-title/page-title.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { Category } from '@/app/models/jcms/category';
+import { LoginService } from '@/app/services/login.service';
 
 @Component({
   selector: 'app-page-title',
@@ -11,5 +12,12 @@ export class PageTitleComponent {
   @Input()
   curentCat: Category | undefined;
 
-  constructor() {}
+  constructor(public login: LoginService) {}
+
+  public getProfileImg(): string {
+    if (this.login.isLogged && this.login.profil && this.login.profil.photo) {
+      return this.login.profil.photo;
+    }
+    return 'assets/images/svg/icone-profil.svg';
+  }
 }
