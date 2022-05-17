7f01e99f33e295b36f0287f6d080838a1cc4ba31
paul604 <paul604@users.noreply.github.com>
2022-05-17 15:36:55 +0200

    form create new alert

--- src/app/Interceptors/jcms-backend.interceptor.ts
+++ src/app/Interceptors/jcms-backend.interceptor.ts
@@ -5,9 +5,10 @@ import {
   HttpInterceptor,
   HttpParams,
   HttpRequest,
+  HttpResponse,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 import { LoginService } from '../services/login.service';
 
@@ -19,6 +20,8 @@ export class JcmsBackendInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
+    // TODO if jcms url
+
     let key = this._login.token;
 
     const newReq = request.clone({
@@ -30,8 +33,26 @@ export class JcmsBackendInterceptor implements HttpInterceptor {
         'authKey',
         key
       ),
+      responseType: 'text', // FIX 201 return no JSON body
     });
 
-    return next.handle(newReq);
+    return next.handle(newReq).pipe(
+      map((response) => {
+        // FIX 201 return no JSON body
+        if (response instanceof HttpResponse) {
+          let body = response.body;
+          if (response.body && response.body !== '') {
+            try {
+              body = JSON.parse(response.body);
+              response = response.clone<any>({ body: body });
+            } catch (e) {
+              console.debug('error parse JSON rep : ' + e);
+            }
+          }
+        }
+
+        return response;
+      })
+    );
   }
 }
--- src/app/pages/new-alert/new-alert.component.ts
+++ src/app/pages/new-alert/new-alert.component.ts
@@ -1,5 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+import { Alerte } from '@/app/models/alerte';
+import { JcmsClientService } from '@/app/services/jcms-client.service';
 import { LabelMngService } from '@/app/services/label-mng.service';
 
 @Component({
@@ -8,9 +11,79 @@ import { LabelMngService } from '@/app/services/label-mng.service';
   styleUrls: ['./new-alert.component.less'],
 })
 export class NewAlertComponent implements OnInit {
-  constructor(public lblService: LabelMngService) {}
+  private _event?: Alerte;
+
+  //fields
+  subject: string = '';
+
+  dateDay: string = '';
+  dateMonth: string = '';
+  dateYear: string = '';
+
+  comment: string = '';
+
+  addCalendar: boolean = false;
+
+  sendAlert: boolean = true;
+
+  constructor(
+    public lblService: LabelMngService,
+    private _jcms: JcmsClientService,
+    private _location: Location
+  ) {}
 
   ngOnInit(): void {
     console.log('TODO new alert');
+  }
+
+  public newAlert() {
+    if (!this.subject || !this.dateYear || !this.dateMonth || !this.dateDay) {
+      // TODO error DS
+      return;
+    }
+
+    //TODO date after day
+
+    this._event = new Alerte(
+      this.subject,
+      this.dateDay,
+      this.dateMonth,
+      this.dateYear,
+      this.comment,
+      this.sendAlert
+    );
+
+    this.saveAlert();
+
+    this.createIcs();
+  }
+
+  private saveAlert() {
+    if (this._event) {
+      let urlEncodedData = this._jcms.encodeParamForBody(
+        this._event.buildForApi()
+      );
+
+      this._jcms.post('data/AlerteZelli', urlEncodedData).subscribe({
+        next: (rep) => {
+          this._location.back();
+        },
+        error: (error) => {
+          console.error(error);
+
+          // TODO error DS
+        },
+      });
+    }
+  }
+
+  private createIcs() {
+    if (!this.addCalendar) {
+      return;
+    }
+    let blob = this._event?.createIcs();
+    if (blob) {
+      window.open(window.URL.createObjectURL(blob));
+    }
   }
 }
