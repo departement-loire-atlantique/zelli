e7b490e35e8831416d6fe0ef8f07491a457663bd
paul604 <paul604@users.noreply.github.com>
2022-07-01 16:22:09 +0200

    Fix env R7

--- src/environments/environment.dev.jcms.R7.ts
+++ src/environments/environment.dev.jcms.R7.ts
@@ -1,20 +1,10 @@
 // Dev with JCMS R7
 
-// export const environment = {
-//   production: false,
-//   urlJcms: 'https://cd44.nantes.trsb.net/',
-//   apiKey:
-//     'djI7cnplbGxpXzEzOTQ2NTM7MTY4MTM3MjgyNDAyODtHRVQsUFVULFBPU1QsREVMRVRFLDs7MjM7JDJhJDA0JDZKdUFpRnFOZzJlTC5OS3Q1WEI4VS5CbWEvSGt0ZEFoZFBndTkwWmpXUW1UVHdrREsuTXV5',
-//   catNavMain: 'rzelli_1394658',
-//   catIntro: 'rzelli_1394827',
-//   catMainContent: 'c_1154085',
-// };
-
 export const environment = {
   production: false,
-  urlJcms: 'http://10.1.12.82/',
+  urlJcms: 'https://cd44.nantes.trsb.net/',
   apiKey:
-    'djI7cnplbGxpXzEzOTQ2NTM7MTY4NjMwNDY2MjkyNTtHRVQsUFVULFBPU1QsREVMRVRFLDs7MTg7JDJhJDA0JDBWRmFJcm5MazNkNG9lM0lHdDJBMmVXVmVaLlouRFFQQlBOMVZhNHRvRnZEVjg3Y3ZCWEtl',
+    'djI7cnplbGxpXzEzOTQ2NTM7MTY4MTM3MjgyNDAyODtHRVQsUFVULFBPU1QsREVMRVRFLDs7MjM7JDJhJDA0JDZKdUFpRnFOZzJlTC5OS3Q1WEI4VS5CbWEvSGt0ZEFoZFBndTkwWmpXUW1UVHdrREsuTXV5',
   catNavMain: 'rzelli_1394658',
   catIntro: 'rzelli_1394827',
   catMainContent: 'c_1154085',
