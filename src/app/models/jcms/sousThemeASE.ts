57bf17f6ab20eb3dde81021d359c89b1a08d2c42
ogaraud <orianne.garaud@digiwin.fr>
2022-07-04 10:24:08 +0200

    ZEL-33 icone subTheme

--- src/app/models/jcms/sousThemeASE.ts
+++ src/app/models/jcms/sousThemeASE.ts
@@ -5,13 +5,15 @@ import { Content } from "./content";
  * Représente le type JCMS "Sous-thème ASE"
  */
 export interface SubThemeASE extends Content {
-  
+
   chapo: string,
-  
+
   /**
    * ArticleASE ou Structure
    */
   contenu: Content[],
+
+  categories: Content[],
 
   affichagePageAAidee: boolean,
 
