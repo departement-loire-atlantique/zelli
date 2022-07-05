1c59495989cbd09b1e2020795290a6e1c1532dcb
ogaraud <orianne.garaud@digiwin.fr>
2022-07-05 11:00:23 +0200

    fix env preprod

--- src/environments/environment.pre.prod.ts
+++ src/environments/environment.pre.prod.ts
@@ -6,4 +6,21 @@ export const environment = {
   catNavMain: 'c_5020',
   catIntro: 'c_5051',
   catMainContent: 'c_5057',
+  catQuestions: 'rzelli_1394678',
+  catThemes: 'rzelli_1394659',
+  catAskQuestionRef: 'rzelli_1394928',
+  catAskQuestionForm: 'rzelli_1394929',
+  catAskQuestionSend: 'rzelli_1394930',
+  firebase: {
+    apiKey: 'AIzaSyD3n-pKkHumsQnVovVgf_FpEBYbKKWDhx8',
+    authDomain: 'zelli-local.firebaseapp.com',
+    projectId: 'zelli-local',
+    storageBucket: 'zelli-local.appspot.com',
+    messagingSenderId: '308279147291',
+    appId: '1:308279147291:web:ffddaba7cfdfb3b39cf1be',
+    measurementId: 'G-WH4LC8SL31',
+
+    vapidKey:
+      'BKu5WfJU0SoDOojTZ0b-tUIZbOBQK9pNsVKDiySdTPMd-8e2sSoYKT_ee10jR86hWgTTl10XsFBXJZenGfsng3w',
+  },
 };
