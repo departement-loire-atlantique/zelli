eaac04fe0a3b8b1707bca122072ed13b74cb4b19
ASSI Franck <franck.assi@loire-atlantique.fr>
2022-11-24 12:11:30 +0100

    Update apiKey JPlatform for PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   urlJcms: 'https://admin-zelli.loire-atlantique.fr/',
   apiKey:
-    'djI7Y181MDE4OzE3MDA4MTQ2ODI1NDE7R0VULFBVVCxQT1NULERFTEVURSw7OzQ1OyQyYSQwNCRaVk1jVkw2dGMvdThrMHhIeEh6SXJPOFJmNk92dm5ZS1lMSnNjN1FCTC9RWEpkUTZmMDdDeQ==',
+    'djI7Y181MDE4OzE3MDA4MjQyMDk3NjY7R0VULFBVVCxQT1NULERFTEVURSw7OzQ1OyQyYSQwNCROTmpzUHJBc2U4aXlNZDJERXYxM0RPM1FRTmJ0L0xNRzN3SHNhUUpCVnRhMmRiVE5FRUsvaQ==',
   catNavMain: 'c_5020',
   catIntro: 'c_5051',
   catMainContent: 'c_5057',
