6716d5e26e8edc33896c525b07daee7db218a6de
paul604 <paul604@users.noreply.github.com>
2022-12-01 10:39:55 +0100

    Fix env R7

--- src/environments/environment.R7.ts
+++ src/environments/environment.R7.ts
@@ -3,30 +3,31 @@ export const environment = {
   urlJcms: 'https://admin-rec-zelli.loire-atlantique.fr/',
   apiKey:
     'djI7Y181MDE4OzE3MDExOTExNjU1ODE7R0VULDsuKjs0ODskMmEkMDQkT2RHRGlBaks0QnEwcXlydUpRa3ZxdWoycFdnaTEzM2lDeWdncmJNUHRsMllBZktJUFFsVU8=',
-    catNavMain: 'c_5020',
-    catIntro: 'c_5051',
-    catMainContent: 'c_5057',
-    catQuestions: 'c_5048',
-    catContact: 'c_5047',
-    catThemes: 'c_5021',
-    catAskQuestionRef: 'c_6287',
-    catAskQuestionForm: 'c_6288',
-    catAskQuestionSend: 'c_6289',
-    catExcludeSearch: 'c_6732',
-    firebase: {
-      apiKey: 'AIzaSyBvxgz4krx7lkor3MBJXwCW0DMXUWpQFi4',
-      authDomain: 'zelli-740a4.firebaseapp.com',
-      projectId: 'zelli-740a4',
-      storageBucket: 'zelli-740a4.appspot.com',
-      messagingSenderId: '531858185893',
-      appId: '1:531858185893:web:ae62fc267871a78811774c',
-      measurementId: 'G-MD67YGW81M',
-  
-      vapidKey:
-        'BBnnYtbvBAtvBiqBvtmGOZVAwpf9Mehd4ghrxf9fg3HPyl-uP-0ZNpYQWae5WRQiyNPIYhk7e3iYg22DrDETZdM',
-    },
-    features: {
-      contacts: false,
-      customAlerts: false,
-    },
+  catNavMain: 'c_5020',
+  catIntro: 'c_5051',
+  catMainContent: 'c_5057',
+  catQuestions: 'c_5048',
+  catContact: 'c_5047',
+  catThemes: 'c_5021',
+  catAskQuestionRef: 'c_6287',
+  catAskQuestionForm: 'c_6288',
+  catAskQuestionSend: 'c_6289',
+  catExcludeSearch: 'c_6732',
+  firebase: {
+    apiKey: 'AIzaSyBvxgz4krx7lkor3MBJXwCW0DMXUWpQFi4',
+    authDomain: 'zelli-740a4.firebaseapp.com',
+    projectId: 'zelli-740a4',
+    storageBucket: 'zelli-740a4.appspot.com',
+    messagingSenderId: '531858185893',
+    appId: '1:531858185893:web:ae62fc267871a78811774c',
+    measurementId: 'G-MD67YGW81M',
+
+    vapidKey:
+      'BBnnYtbvBAtvBiqBvtmGOZVAwpf9Mehd4ghrxf9fg3HPyl-uP-0ZNpYQWae5WRQiyNPIYhk7e3iYg22DrDETZdM',
+  },
+  features: {
+    contacts: false,
+    customAlerts: false,
+    editPhoto: false,
+  },
 };
